8367eb03f03e723965ca46bdc6a5746895a1873c
unknown <matthewpeterkort@gmail.com>
2023-03-05 08:21:06 -0800

    lint

--- next.config.js
+++ next.config.js
@@ -4,22 +4,9 @@ const withNextra = require('nextra')({
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  // any configs you need
 }
 
 module.exports = withNextra(nextConfig)
