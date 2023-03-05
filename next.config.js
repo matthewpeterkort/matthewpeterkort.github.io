c5ee6b20c6cd8ff2af0d4a8ccfb3079406fd1702
unknown <matthewpeterkort@gmail.com>
2023-03-05 08:54:30 -0800

    lint

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.js',
+  
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  
   // any configs you need
 }
 
